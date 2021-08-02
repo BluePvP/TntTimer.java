44bad77f6525c13a3ee711e72251397fde1e5b6e
ChronsWrld <85413934+ChronsWrld@users.noreply.github.com>
2021-08-02 12:46:52 +0100

    Update TntTimer.java

--- TntTimer.java
+++ TntTimer.java
@@ -1,18 +1,49 @@
-/*
-Original Author - Ski1er
-then what did i done - idk
-*/
+public void doRender(EntityTNTPrimed entity, double x, double y, double z, float entityYaw, float partialTicks)
+    {
+        BlockRendererDispatcher blockrendererdispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
+        this.renderTag(this, entity, x, y, z, partialTicks);
+        GlStateManager.pushMatrix();
+        GlStateManager.translate((float)x, (float)y + 0.5F, (float)z);
 
+        if ((float)entity.fuse - partialTicks + 1.0F < 10.0F)
+        {
+            float f = 1.0F - ((float)entity.fuse - partialTicks + 1.0F) / 10.0F;
+            f = MathHelper.clamp_float(f, 0.0F, 1.0F);
+            f = f * f;
+            f = f * f;
+            float f1 = 1.0F + f * 0.3F;
+            GlStateManager.scale(f1, f1, f1);
+        }
 
+        float f2 = (1.0F - ((float)entity.fuse - partialTicks + 1.0F) / 100.0F) * 0.8F;
+        this.bindEntityTexture(entity);
+        GlStateManager.translate(-0.5F, -0.5F, 0.5F);
+        blockrendererdispatcher.renderBlockBrightness(Blocks.tnt.getDefaultState(), entity.getBrightness(partialTicks));
+        GlStateManager.translate(0.0F, 0.0F, 1.0F);
 
-//go to renderTNT primed class
-  
- //add this to doRender method
-   this.renderTag(this, entity, x, y, z, partialTicks);
+        if (entity.fuse / 5 % 2 == 0)
+        {
+            GlStateManager.disableTexture2D();
+            GlStateManager.disableLighting();
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(770, 772);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, f2);
+            GlStateManager.doPolygonOffset(-3.0F, -3.0F);
+            GlStateManager.enablePolygonOffset();
+            blockrendererdispatcher.renderBlockBrightness(Blocks.tnt.getDefaultState(), 1.0F);
+            GlStateManager.doPolygonOffset(0.0F, 0.0F);
+            GlStateManager.disablePolygonOffset();
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.disableBlend();
+            GlStateManager.enableLighting();
+            GlStateManager.enableTexture2D();
+        }
 
-//then add this method under doRender method;-
-  
-   public void renderTag(RenderTNTPrimed tntRenderer, EntityTNTPrimed tntPrimed, double x, double y, double z, float partialTicks) {
+        GlStateManager.popMatrix();
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
+    }
+    
+    public void renderTag(RenderTNTPrimed tntRenderer, EntityTNTPrimed tntPrimed, double x, double y, double z, float partialTicks) {
         if (tntPrimed.fuse >= 1) {
             double d0 = tntPrimed.getDistanceSqToEntity(tntRenderer.getRenderManager().livingPlayer);
 
